20d6a6fd2b7188b875c80a0603489b37907a9dbc
pesta <andru.pestov@gmail.com>
2020-05-01 17:35:27 +0300

    push на стат

--- Pages/StatPage.Designer.cs
+++ Pages/StatPage.Designer.cs
@@ -39,10 +39,13 @@
             this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
             this.metroTile3 = new MetroFramework.Controls.MetroTile();
             this.metroTile2 = new MetroFramework.Controls.MetroTile();
+            this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
+            this.label1 = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
             this.tableLayoutPanel2.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.tableLayoutPanel1.SuspendLayout();
+            this.flowLayoutPanel1.SuspendLayout();
             this.SuspendLayout();
             // 
             // openFileDialog1
@@ -55,19 +58,19 @@
             this.MainPush.BackColor = System.Drawing.SystemColors.Highlight;
             this.MainPush.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
             this.MainPush.ForeColor = System.Drawing.SystemColors.Control;
-            this.MainPush.Location = new System.Drawing.Point(24, 729);
-            this.MainPush.Margin = new System.Windows.Forms.Padding(4, 0, 5, 0);
+            this.MainPush.Location = new System.Drawing.Point(28, 720);
+            this.MainPush.Margin = new System.Windows.Forms.Padding(5, 0, 7, 0);
             this.MainPush.Name = "MainPush";
-            this.MainPush.Size = new System.Drawing.Size(0, 17);
+            this.MainPush.Size = new System.Drawing.Size(0, 20);
             this.MainPush.TabIndex = 4;
             // 
             // pictureBox2
             // 
             this.pictureBox2.BackColor = System.Drawing.SystemColors.Highlight;
-            this.pictureBox2.Location = new System.Drawing.Point(11, 722);
-            this.pictureBox2.Margin = new System.Windows.Forms.Padding(4);
+            this.pictureBox2.Location = new System.Drawing.Point(15, 889);
+            this.pictureBox2.Margin = new System.Windows.Forms.Padding(5);
             this.pictureBox2.Name = "pictureBox2";
-            this.pictureBox2.Size = new System.Drawing.Size(1469, 34);
+            this.pictureBox2.Size = new System.Drawing.Size(1959, 42);
             this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.pictureBox2.TabIndex = 5;
             this.pictureBox2.TabStop = false;
@@ -81,12 +84,12 @@
             this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 20.28591F));
             this.tableLayoutPanel2.Controls.Add(this.pictureBox1, 0, 0);
             this.tableLayoutPanel2.Controls.Add(this.loginButton, 3, 0);
-            this.tableLayoutPanel2.Location = new System.Drawing.Point(0, 69);
-            this.tableLayoutPanel2.Margin = new System.Windows.Forms.Padding(2);
+            this.tableLayoutPanel2.Location = new System.Drawing.Point(0, 85);
+            this.tableLayoutPanel2.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.tableLayoutPanel2.Name = "tableLayoutPanel2";
             this.tableLayoutPanel2.RowCount = 1;
             this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
-            this.tableLayoutPanel2.Size = new System.Drawing.Size(1125, 93);
+            this.tableLayoutPanel2.Size = new System.Drawing.Size(1500, 114);
             this.tableLayoutPanel2.TabIndex = 6;
             // 
             // pictureBox1
@@ -94,10 +97,10 @@
             this.tableLayoutPanel2.SetColumnSpan(this.pictureBox1, 2);
             this.pictureBox1.Dock = System.Windows.Forms.DockStyle.Fill;
             this.pictureBox1.Image = global::SmartFactory.Properties.Resources.Logo2;
-            this.pictureBox1.Location = new System.Drawing.Point(8, 2);
-            this.pictureBox1.Margin = new System.Windows.Forms.Padding(8, 2, 2, 2);
+            this.pictureBox1.Location = new System.Drawing.Point(11, 2);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(11, 2, 3, 2);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(281, 89);
+            this.pictureBox1.Size = new System.Drawing.Size(374, 110);
             this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
             this.pictureBox1.TabIndex = 0;
             this.pictureBox1.TabStop = false;
@@ -105,10 +108,10 @@
             // loginButton
             // 
             this.loginButton.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.loginButton.Location = new System.Drawing.Point(914, 20);
-            this.loginButton.Margin = new System.Windows.Forms.Padding(19, 20, 19, 20);
+            this.loginButton.Location = new System.Drawing.Point(1218, 25);
+            this.loginButton.Margin = new System.Windows.Forms.Padding(25);
             this.loginButton.Name = "loginButton";
-            this.loginButton.Size = new System.Drawing.Size(192, 53);
+            this.loginButton.Size = new System.Drawing.Size(257, 64);
             this.loginButton.TabIndex = 1;
             this.loginButton.Text = "Вход";
             this.loginButton.Theme = MetroFramework.MetroThemeStyle.Dark;
@@ -116,10 +119,10 @@
             // 
             // metroTile1
             // 
-            this.metroTile1.Location = new System.Drawing.Point(371, 2);
-            this.metroTile1.Margin = new System.Windows.Forms.Padding(2);
+            this.metroTile1.Location = new System.Drawing.Point(495, 2);
+            this.metroTile1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.metroTile1.Name = "metroTile1";
-            this.metroTile1.Size = new System.Drawing.Size(364, 197);
+            this.metroTile1.Size = new System.Drawing.Size(485, 242);
             this.metroTile1.TabIndex = 7;
             this.metroTile1.Text = "Данные со всех устроуйств";
             this.metroTile1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
@@ -129,10 +132,10 @@
             // 
             // metroButton1
             // 
-            this.metroButton1.Location = new System.Drawing.Point(378, 449);
-            this.metroButton1.Margin = new System.Windows.Forms.Padding(2);
+            this.metroButton1.Location = new System.Drawing.Point(504, 553);
+            this.metroButton1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.metroButton1.Name = "metroButton1";
-            this.metroButton1.Size = new System.Drawing.Size(364, 65);
+            this.metroButton1.Size = new System.Drawing.Size(485, 80);
             this.metroButton1.TabIndex = 8;
             this.metroButton1.Text = "Загрузка данных телеметрии";
             this.metroButton1.Click += new System.EventHandler(this.button6_Click);
@@ -146,19 +149,20 @@
             this.tableLayoutPanel1.Controls.Add(this.metroTile1, 1, 0);
             this.tableLayoutPanel1.Controls.Add(this.metroTile3, 2, 0);
             this.tableLayoutPanel1.Controls.Add(this.metroTile2, 0, 0);
-            this.tableLayoutPanel1.Location = new System.Drawing.Point(8, 205);
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(11, 252);
+            this.tableLayoutPanel1.Margin = new System.Windows.Forms.Padding(4);
             this.tableLayoutPanel1.Name = "tableLayoutPanel1";
             this.tableLayoutPanel1.RowCount = 1;
             this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
-            this.tableLayoutPanel1.Size = new System.Drawing.Size(1107, 201);
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(1476, 247);
             this.tableLayoutPanel1.TabIndex = 9;
             // 
             // metroTile3
             // 
-            this.metroTile3.Location = new System.Drawing.Point(740, 2);
-            this.metroTile3.Margin = new System.Windows.Forms.Padding(2);
+            this.metroTile3.Location = new System.Drawing.Point(987, 2);
+            this.metroTile3.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.metroTile3.Name = "metroTile3";
-            this.metroTile3.Size = new System.Drawing.Size(365, 197);
+            this.metroTile3.Size = new System.Drawing.Size(486, 242);
             this.metroTile3.TabIndex = 9;
             this.metroTile3.Text = "Устройства в критическом состоянии";
             this.metroTile3.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
@@ -168,10 +172,10 @@
             // 
             // metroTile2
             // 
-            this.metroTile2.Location = new System.Drawing.Point(2, 2);
-            this.metroTile2.Margin = new System.Windows.Forms.Padding(2);
+            this.metroTile2.Location = new System.Drawing.Point(3, 2);
+            this.metroTile2.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.metroTile2.Name = "metroTile2";
-            this.metroTile2.Size = new System.Drawing.Size(364, 197);
+            this.metroTile2.Size = new System.Drawing.Size(485, 242);
             this.metroTile2.TabIndex = 8;
             this.metroTile2.Text = "Опасные устройства";
             this.metroTile2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
@@ -179,27 +183,54 @@
             this.metroTile2.TileTextFontWeight = MetroFramework.MetroTileTextWeight.Bold;
             this.metroTile2.Click += new System.EventHandler(this.metroTile2_Click);
             // 
+            // flowLayoutPanel1
+            // 
+            this.flowLayoutPanel1.BackColor = System.Drawing.SystemColors.Highlight;
+            this.flowLayoutPanel1.Controls.Add(this.label1);
+            this.flowLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.flowLayoutPanel1.Location = new System.Drawing.Point(20, 715);
+            this.flowLayoutPanel1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.flowLayoutPanel1.Name = "flowLayoutPanel1";
+            this.flowLayoutPanel1.Size = new System.Drawing.Size(1460, 31);
+            this.flowLayoutPanel1.TabIndex = 10;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.SystemColors.Highlight;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.ForeColor = System.Drawing.SystemColors.Control;
+            this.label1.Location = new System.Drawing.Point(4, 0);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(0, 29);
+            this.label1.TabIndex = 2;
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
             // StatPage
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1125, 622);
+            this.ClientSize = new System.Drawing.Size(1500, 766);
+            this.Controls.Add(this.flowLayoutPanel1);
             this.Controls.Add(this.metroButton1);
             this.Controls.Add(this.tableLayoutPanel2);
             this.Controls.Add(this.MainPush);
             this.Controls.Add(this.pictureBox2);
             this.Controls.Add(this.tableLayoutPanel1);
-            this.Margin = new System.Windows.Forms.Padding(2);
-            this.MaximumSize = new System.Drawing.Size(1125, 650);
-            this.MinimumSize = new System.Drawing.Size(1076, 622);
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.MaximumSize = new System.Drawing.Size(1500, 800);
+            this.MinimumSize = new System.Drawing.Size(1435, 766);
             this.Name = "StatPage";
-            this.Padding = new System.Windows.Forms.Padding(15, 60, 15, 16);
+            this.Padding = new System.Windows.Forms.Padding(20, 74, 20, 20);
             this.Text = "Телеметрия";
             this.Load += new System.EventHandler(this.StatPage_Load);
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
             this.tableLayoutPanel2.ResumeLayout(false);
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.tableLayoutPanel1.ResumeLayout(false);
+            this.flowLayoutPanel1.ResumeLayout(false);
+            this.flowLayoutPanel1.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -217,5 +248,7 @@
         private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
         private MetroFramework.Controls.MetroTile metroTile3;
         private MetroFramework.Controls.MetroTile metroTile2;
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel1;
+        public System.Windows.Forms.Label label1;
     }
 }
